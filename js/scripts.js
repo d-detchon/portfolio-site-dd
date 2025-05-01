57c6158eddc3d2daf45b39a41a752b1d7de82c6c
d-detchon <detchon.d@gmail.com>
2025-05-01 10:20:57 -0600

    code corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+
+const deborah = {
+    name: "Deborah",
+    age: 34,
+    hobbies: ["listening to new country", "swimming", "Zen"]
+  };
+  
+console.log(deborah);
+
+console.log(deborah.hobbies[0]); // shows "drawing"
+console.log(deborah.name);       // shows "Deborah"
