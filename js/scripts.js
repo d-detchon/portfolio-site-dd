a50a168657b1f015988ea0daafcd745f78c58518
d-detchon <detchon.d@gmail.com>
2025-05-01 15:15:12 -0600

    adjust with flex box added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
-
 function menuToggle() {
-  // Only toggle if screen is less than 640px wide
-  if (window.innerWidth < 640) {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
 }
